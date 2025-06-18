3630c0d7604267f3fbe02dbc615a97717c81c8fa
PlanetCentral <62634066+PlanetCentral@users.noreply.github.com>
2025-06-17 18:23:19 -0700

    Update query3.sql

--- Lab2/query3.sql
+++ Lab2/query3.sql
@@ -1,1 +1,6 @@
-
+select o.supplierID as theSupplierID
+from OrderSupply o
+left join Supplier s on o.supplierID = s.supplierID
+where s.supplierName!='McKesson'
+group by o.supplierID, o.pharmacyID 
+having count(o.drugID)>=2; 

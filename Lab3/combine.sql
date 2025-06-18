de5cda7398894152b246eb76c5529827a96bc765
PlanetCentral <62634066+PlanetCentral@users.noreply.github.com>
2025-06-17 18:24:08 -0700

    Update combine.sql

--- Lab3/combine.sql
+++ Lab3/combine.sql
@@ -1,1 +1,17 @@
+BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE;
+
+INSERT INTO Supplier(supplierID, supplierName, rating)
+SELECT us.supplierID, us.supplierName, 9
+FROM UpgradeSupplier us
+WHERE NOT EXISTS (
+  SELECT 1 FROM Supplier s WHERE s.supplierID = us.supplierID
+);
+
+UPDATE Supplier s
+SET supplierName = us.supplierName,
+    rating = s.rating + 1
+FROM UpgradeSupplier us
+WHERE s.supplierID = us.supplierID;
+
+COMMIT;
 

c3448e6bee28a0dd04037358b2b5e421c173f095
PlanetCentral <62634066+PlanetCentral@users.noreply.github.com>
2025-06-17 18:22:49 -0700

    Update query1.sql

--- Lab2/query1.sql
+++ Lab2/query1.sql
@@ -1,1 +1,5 @@
-
+select customerName, totalPrice, Pharmacy.address as pharmacyAddress
+FROM Lab2.Customer
+left join lab2.purchase on Customer.customerID = Purchase.customerID
+left join lab2.pharmacy on Pharmacy.pharmacyID = Purchase.pharmacyID
+WHERE customerName LIKE 'R%' AND totalPrice>40; --anything starts with R, otherwise youd do it before e.g. %R

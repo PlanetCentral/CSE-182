61b96bfc080a5867ad26bd72cf85de8a95289366
PlanetCentral <62634066+PlanetCentral@users.noreply.github.com>
2025-06-17 18:23:43 -0700

    Update query5.sql

--- Lab2/query5.sql
+++ Lab2/query5.sql
@@ -1,1 +1,7 @@
-
+Select distinct pharmacyID as thePharmacyID, OrderSupply.supplierID AS theSupplierID
+from OrderSupply
+inner join(
+Select supplierID, min(orderDate) as orderDate
+from OrderSupply
+group by supplierID) 
+AS minOrderDate on OrderSupply.supplierID=minOrderDate.supplierID and OrderSupply.orderDate=minOrderDate.orderDate;

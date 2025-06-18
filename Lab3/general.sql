464dc4719077bd436f23d01f4adb2a717bf1d354
PlanetCentral <62634066+PlanetCentral@users.noreply.github.com>
2025-06-17 18:25:17 -0700

    Update general.sql

--- Lab3/general.sql
+++ Lab3/general.sql
@@ -1,1 +1,6 @@
+ALTER TABLE Pharmacy ADD CONSTRAINT pharmacyEmployeesPositive CHECK (numEmployees > 0);
 
+ALTER TABLE OrderSupply ADD CONSTRAINT validOrderStatus CHECK (status IN ('dlvd', 'pndg', 'cnld') OR status IS NULL);
+
+ALTER TABLE Purchase ADD CONSTRAINT ifNullTypeThenNullNumber CHECK ( creditCardType IS NOT NULL 
+OR creditCardNumber IS NULL);

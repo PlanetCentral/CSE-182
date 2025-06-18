d51e51f177bdc8c2c855682dec5f011f0b6c4057
PlanetCentral <62634066+PlanetCentral@users.noreply.github.com>
2025-06-17 18:23:01 -0700

    Update query2.sql

--- Lab2/query2.sql
+++ Lab2/query2.sql
@@ -1,1 +1,7 @@
-
+select distinct c.customerID, c.address
+from Customer c
+left join Lab2.Purchase p on c.customerID = p.customerID
+where p.creditCardType='V' and p.customerID not in (select distinct customerID
+from Purchase 
+where totalPrice>100)
+order by c.address desc, c.customerID asc;

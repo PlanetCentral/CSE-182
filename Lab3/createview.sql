97e4995577c430cfa2654112ff910dd20955517e
PlanetCentral <62634066+PlanetCentral@users.noreply.github.com>
2025-06-17 18:24:49 -0700

    Update createview.sql

--- Lab3/createview.sql
+++ Lab3/createview.sql
@@ -1,1 +1,22 @@
+CREATE VIEW BadTotalPriceView AS
+SELECT p.purchaseID,
+       p.customerID,
+       p.pharmacyID,
+       p.purchaseTimestamp,
+       p.totalPrice,
+       p.creditCardType,
+       p.creditCardNumber,
+       p.expirationDate,
+       SUM(dip.subtotal) AS totalDrugPrice
+FROM Purchase p
+JOIN DrugsInPurchase dip ON p.purchaseID = dip.purchaseID
+GROUP BY p.purchaseID,
+         p.customerID,
+         p.pharmacyID,
+         p.purchaseTimestamp,
+         p.totalPrice,
+         p.creditCardType,
+         p.creditCardNumber,
+         p.expirationDate
+HAVING ABS(p.totalPrice - SUM(dip.subtotal)) >= 1;
 

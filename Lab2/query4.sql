533d649b64d8c6cfd33b3327618535a6222337d4
PlanetCentral <62634066+PlanetCentral@users.noreply.github.com>
2025-06-17 18:23:30 -0700

    Update query4.sql

--- Lab2/query4.sql
+++ Lab2/query4.sql
@@ -1,1 +1,11 @@
-
+SELECT Drug.drugID as theDrugID, Purchase.purchaseTimestamp as thePurchaseTimestamp
+FROM Drug
+inner join DrugsInPurchase on Drug.drugID=DrugsInPurchase.drugID
+left join Purchase on DrugsInPurchase.purchaseID=Purchase.purchaseID
+left join Pharmacy on Purchase.pharmacyID=Pharmacy.pharmacyID
+WHERE prescriptionRequired=TRUE 
+AND DATE(purchaseTimestamp)>='2024-01-24' 
+AND Pharmacy.address LIKE '%en%'
+AND Purchase.creditCardNumber IS NULL 
+group by Purchase.purchaseTimestamp, Drug.DrugID
+having SUM(DrugsInPurchase.quantity)>=2;
